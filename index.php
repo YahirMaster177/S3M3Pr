cba44b6a2ea637cb2754bab36aede8bba7ac2b69
YahirMaster177 <100593978+YahirMaster177@users.noreply.github.com>
2022-03-02 03:39:32 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Menu Principal</title>
+</head>
+<body>
+	<h1>Ejercicios de php</h1>
+		<a href="Ejercicio1\datos-poligono.php">Calcula El Area</a><br><br>
+		<a href="Examen\index.php">Examen</a>
+
+
+
+</body>
+</html>
